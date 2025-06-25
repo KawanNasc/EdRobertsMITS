9b1a06a2c6f4243b38a40541e6ae278eeb98f906
KawanNasc <atlaskawan@gmail.com>
2025-06-25 17:58:51 -0300

    Início do englobamento da estilagem interna para externa, centralização e arredondamento dos nomes da equipe e simplificação da nomeação dos estilos

--- scripts/site.js
+++ scripts/site.js
@@ -1,14 +1,14 @@
 let slide = document.querySelectorAll('.slide-container');
 let index = 0;
 
-function seta_d() {
+function click_l() {
     slide[index].classList.remove('active');
-    index = (index + 1) % slide.length;
+    index = (index - 1 + slide.length) % slide.length;
     slide[index].classList.add('active');
 }
 
-function seta_e() {
+function click_r() {
     slide[index].classList.remove('active');
-    index = (index - 1 + slide.length) % slide.length;
+    index = (index + 1) % slide.length;
     slide[index].classList.add('active');
 }
